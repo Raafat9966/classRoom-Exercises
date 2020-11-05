f94e756a3cf1830ad69e3bbc4ff1299e097eb9dd
Raafat <slipknot9966@gmail.com>
2020-11-05 15:05:54 +0100

    add test exercise

---
+++ testing-unit/test-exercise.js
@@ -0,0 +1,9 @@
+const sum = (num1, num2) => num1 + num2;
+const difference = (num1, num2) => num1 - num2;
+const product = (num1, num2) => num1 * num2;
+const quotient = (num1, num2) => num1 / num2;
+const remainder = (num1, num2) => num1 % num2;
+const abs = (num1) => Math.abs(num1);
+const pow = (num1, num2) => num1 ** num2;
+
+module.exports = { difference, sum, product, quotient, remainder, abs, pow };
