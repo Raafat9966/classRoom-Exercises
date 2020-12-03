f7c72427b7a3aa4d3cd5b65f6c12fbcaf8dbbd17
Raafat <slipknot9966@gmail.com>
2020-12-03 18:10:37 +0100

    adding jquery exercise

---
+++ DOM-exercises/exercise-Jquery/index.js
@@ -0,0 +1,8 @@
+// Work on your tasks here
+$(function () {
+	// $("#modal").on("show.bs.modal", function (event) {
+	// 	let modalInstance = $(event.target).data("modal");
+	// 	let modal = $(this);
+	// 	modal.find(".modal").text(`worked ${modalInstance}`);
+	// });
+});
