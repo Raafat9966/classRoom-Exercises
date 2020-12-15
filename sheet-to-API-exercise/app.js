51887b68c10aa1ceff9ac79bb3d927b5291b4be7
Raafat <slipknot9966@gmail.com>
2020-12-15 12:24:35 +0100

    updating

--- sheet-to-API-exercise/app.js
+++ sheet-to-API-exercise/app.js
@@ -4,13 +4,13 @@ const renderSheets = async () => {
 		"https://sheet2api.com/v1/G8m99f5BQ0uj/sheet-to-api/data-sheet"
 	);
 	let data = await respond.json();
-	data.map((vol) => {
+	data.map((val) => {
 		table.innerHTML += `
       <tr>
-        <td>${vol.Date}</td>
-        <td>${vol.High}</td>
-        <td>${vol.Low}</td>
-        <td>${vol.Volume}</td>
+        <td>${val.Date}</td>
+        <td>${val.High}</td>
+        <td>${val.Low}</td>
+        <td>${val.Volume}</td>
       </tr>`;
 	});
 };
