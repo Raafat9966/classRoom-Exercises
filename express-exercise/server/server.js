b601b532018c1a78b0cbdde97ff542f6b2f6bf48
Raafat <slipknot9966@gmail.com>
2021-01-15 09:11:01 +0100

    updating

--- express-exercise/server/server.js
+++ express-exercise/server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 let port = 5000;
 const app = express();
 
 // app.use(express.static(path.join(__dirname, "public")));
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) =>
 	res.status(200).sendFile(path.join(__dirname, "public", "index.html"))
 );
@@ -20,9 +22,19 @@ app.get("/about", (req, res) =>
 // 	res.status(200).send(`<h1>Hello in the About page</h1>`)
 // );
 
-// app.get("/message", (req, res) =>
-// 	res.status(200).send(`<h1>Live free or Die hard</h1>`)
-// );
+app.get("/me", (req, res) =>
+	res.status(200).send(`<h1>Live free or Die hard</h1>`)
+);
+
+app.post("/login", (req, res) => {
+	// console.log(req.body);
+
+	if (req.body.username == "admin" && req.body.password == "secret") {
+		res.status(200).send(" Hi Boss!");
+	} else {
+		res.status(200).send(" I do not know you!!!");
+	}
+});
 
 app.listen(port, () =>
 	console.log(`Server running on host: http://localhost:${port}`)
