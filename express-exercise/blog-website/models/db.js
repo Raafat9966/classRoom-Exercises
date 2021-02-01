3d8023ad0d53826a096d007fd6e032e4c0095f44
Raafat <slipknot9966@gmail.com>
2021-02-01 08:54:22 +0100

    updating

--- express-exercise/blog-website/models/db.js
+++ express-exercise/blog-website/models/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connectionString = "";
+const connectionString = process.env.MONGODB_URI;
 
 const Schema = mongoose.Schema;
 
